b7dccbd54752ae507669840506f0f73715ea597d
SKadiev <kadievstojan@gmail.com>
2022-03-30 19:05:47 +0200

    aaa

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Livewire\Home\IndexHome;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', \App\Http\Livewire\Home\IndexHome::class)->name('home');
+    Route::get('/', IndexHome::class);
     Route::get('/channel/{channel}/edit', \App\Http\Livewire\Channel\EditChannel::class);
 });
