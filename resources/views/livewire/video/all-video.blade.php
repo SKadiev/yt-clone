9c9c2be9e045af17a00bb8d37095d8fe79a71a23
Stojan Kadiev <kadievstojan@gmail.com>
2022-04-07 15:29:48 +0200

    videos listing update

--- resources/views/livewire/video/all-video.blade.php
+++ resources/views/livewire/video/all-video.blade.php
@@ -1,9 +1,11 @@
 <div  xmlns:livewire="http://www.w3.org/1999/html">
-    @foreach($videos as $video)
+    @forelse($videos as $video)
         <li wire:key="video-id-{{ $video->uid }}">
             <livewire:video.video-component :video="$video" wire:key="video-component-{{ $video->uid }}"/>
         </li>
-    @endforeach
+        @empty
+            <h1 class="offset-3">No videos</h1>
+    @endforelse
 {{--    @if(\Route::is('videos.global'))--}}
         <div class="col-md-6 offset-3 pagination-nav">
             {{ $videos->links() }}
